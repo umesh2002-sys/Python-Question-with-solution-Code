2b08eb5bea4bddd0755fdc84ab2a6a98b3fa54ee
umesh2002-sys <umesh.shrestha2002@gmail.com>
2022-07-31 22:27:46 +0545

    Program of checking either user enter number is prime number or not

---
+++ Prime_number.py
@@ -0,0 +1,19 @@
+
+
+from msilib.schema import Condition
+
+
+number = int(input("Enter a number: "))
+
+Condition = False
+
+if number > 1:
+    for i in range(2, number):
+        if(number % i == 0):
+            Condition = True
+            break
+
+if Condition:
+    print(f"{number} is a not prime number")
+else:
+    print(f"{number} is a prime number")
