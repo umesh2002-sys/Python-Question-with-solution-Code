598e60ca6efeee43d1502ccaf482032974dbc60f
umesh2002-sys <umesh.shrestha2002@gmail.com>
2022-08-02 23:05:32 +0545

    commiting same word count

---
+++ count_word/count_word.py
@@ -0,0 +1,13 @@
+text = """
+Meta Platforms, Inc., doing business as Meta and formerly named Facebook, Inc., and TheFacebook, Inc., is an American multinational technology conglomerate based in Menlo Park, California. The company owns Facebook, Instagram, and WhatsApp, among other products and services.
+"""
+
+word_count = {}
+
+for word in text.split():
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
+
+print(word_count)
